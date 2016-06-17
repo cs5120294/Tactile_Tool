b2309395e8f537c0b63d5abb111c2e33b3b7a9b0
Prerit Kumar Patidar <kumarpkp50@gmail.com>
2016-06-17 11:05:18 +0530

    Add files via upload

---
+++ page0.java
@@ -0,0 +1,8 @@
+import javax.swing.JPanel;
+
+public class page0 extends JPanel{
+    public page0(){
+        SCR.a2.pane.add(SCR.a1.Start);
+        SCR.a1.Start.setBounds(250, 450, 200, 30);
+    }
+}
